0f4c3b6848a6689f124cabf09ffde9fde9b18328
Diones Gomes <diones.zack.dz@gmail.com>
2018-04-03 11:59:44 -0300

    Mapa adicionado a página de cadastro!

--- site 2.0/js/formulario.js
+++ site 2.0/js/formulario.js
@@ -51,3 +51,19 @@ else
 return true;
 }
 // <!-- Fim do JavaScript que validará os campos obrigatórios! -->
+
+
+// javascript referente ao mapa
+
+function initMap() { /* A função initMap inicializa e adiciona o mapa quando a página da Web carrega. S*/
+    var local = {lat: -6.883712399999999, lng: -38.5573006}; /* Longitute e latituade */
+    var map = new google.maps.Map(document.getElementById('map'), { /*Adiciona um novo objeto do Google Maps para construir um mapa no elemento div.*/
+       zoom: 4,
+       center: local  /*A propriedade center informa a API a localização do ponto central do mapa. */
+   });
+       var marker = new google.maps.Marker({ /* Adiciona um marcador no mapa e O position propriedade define a posição do marcador.*/
+       position: local,
+       map: map
+   });
+}
+    
