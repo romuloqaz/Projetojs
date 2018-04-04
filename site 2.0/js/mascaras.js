b4989063afede10bbbd5694e1002a74e31aa6166
Raul <raulcc50@gmail.com>
2018-04-04 16:46:43 -0300

    add validação

--- site 2.0/js/mascaras.js
+++ site 2.0/js/mascaras.js
@@ -1,22 +1,15 @@
-$(document).ready(function masktel9d(){
+$(document).ready(function(){
     var maskTel9d = function (val) {
             return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
         },
         options = {onKeyPress: function(val, e, field, options) {
                 field.mask(maskTel9d.apply({}, arguments), options);
             }
-        };
+    };
     $('.telefone').mask(maskTel9d, options);
-});
-
-$(document).ready(function maskcpf(){
+    $('.data').mask('00/00/0000');
+    $('.cepx').mask('00000-000');
     $('.cpf').mask('000.000.000-00', {reverse: true});
-});
-
-$(document).ready(function maskcep(){
-    $('.cepz').mask('00000-000');
-});
-
-$(document).ready(function maskdata(){
-    $('.nascimentz').mask('00/00/0000');
+    $('.numero').mask('0000000');
+    $('.rg').mask('00.000.000-0');
 });
