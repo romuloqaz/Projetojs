bf26b3801168e84fa27c7862e4d5c956e2cce8e9
Raul <raulcc50@gmail.com>
2018-04-04 14:23:39 -0300

    comentando js

--- site 2.0/js/formulario.js
+++ site 2.0/js/formulario.js
@@ -39,6 +39,7 @@ function initMap() {
 // ARMAZENAMENTO
 
 function verificarCadastro(){
+	// PEGA OS VALORES DE CADA INPUT
     var nome_Usuario = document.getElementById('nome').value;
     var cpf_Usuario = document.getElementById('cpf').value;
     var rua_Usuario = document.getElementById('rua').value;
@@ -51,6 +52,8 @@ function verificarCadastro(){
     var email_Usuario = document.getElementById('email').value;
     var senha_Usuario = document.getElementById('senha').value;
     var foto_Usuario = document.getElementById('foto_perfil').value;
+
+    // TESTA SE OS CAMPOS ESTAO PREENchidoS OU NAO
     if((nome_Usuario != "") && (cpf_Usuario!="") && (rua_Usuario!="") && (data_Nasc_Usuario!="") && (bairro_Usuario!="")&&(numero_Usuario!="")&&(telefone_Usuario!="")&&(cep_Usuario!="")&&(rg_Usuario!="")&&(email_Usuario!="")&&(senha_Usuario!="")){
         armazenarDados(email_Usuario,senha_Usuario);
     }else{
@@ -58,35 +61,37 @@ function verificarCadastro(){
     }
 }
 
-function armazenarDados(email_Usuario,senha_Usuario){
-    var info = [email_Usuario,senha_Usuario];
-    localStorage.setItem(email_Usuario,info);
+function armazenarDados(email_Usuario,senha_Usuario){ // Passo os parametros que vao ser armazenados
+    var info = [email_Usuario,senha_Usuario];			// Armazenando o EMAIL e a SENHA do usuario
+    localStorage.setItem(email_Usuario,info);			// Guardando o Info no localstorage
     console.log(localStorage.getItem(email_Usuario));
 }
 
-function verificarChave(usuario){
-    var senha = prompt("Digite sua senha");
-    console.log(usuario);
-    if(senha == usuario[1]){
-        alert("Usuario logado");
+function verificarChave(usuario){			// Passo o parametro usuario que contem o vetor com duas posicoes
+    var senha = prompt("Digite sua senha");	// Informar a senha e armazena-la
+    console.log(usuario);					
+    if(senha == usuario[1]){				// Verifica se a senha confere com a senha do parametro usuario
+        alert("Usuario logado");			// Se conferir o Usuario está logado
     }else{
-        alert("Senha incorreta");
+        alert("Senha incorreta");           // Caso Nao estejá o Usuario errou a senha
         verificarChave(usuario);
     }
 }
 
 function login(){
-    var chave = prompt("Digite seu email");
-    var usuario = localStorage.getItem(chave);
-    if(usuario!=null){
-        usuario = usuario.split(',');        
-        verificarChave(usuario);
+    var chave = prompt("Digite seu email");  // Pega uma chave de acesso (EMAIL)
+    var usuario = localStorage.getItem(chave);	// Guarda em uma variavel (USUARIO) o item do localstorage (CHAVE = EMAIL)
+    if(usuario!=null){							// Verifica se o USUARIO existe
+        usuario = usuario.split(',');        	// Se existir : como no vetor e separado por virgula SPLIT separa em duas posicoes (0,1) referente a EMAIL e SENHA
+        verificarChave(usuario);				// Chama a Funcao de verificar se a senha está correta
     }else{
         alert("Usuario não cadastrado");
         login();
     }
 }
-  //imagem do formulário
+
+
+ //imagem do formulário
 $(function () {
             $(document).on("change", ".Upload", function(e) {
                 showThumbnail(this.files);
